cd506ce4b20a4d6e4d82eb5c2b10f0b4960b4f98
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-13 16:47:16 +0100

    calculate total price

--- src/main/java/com/capgemini/Register.java
+++ src/main/java/com/capgemini/Register.java
@@ -11,13 +11,19 @@ public class Register implements IRegister {
 
     public void checkOut(Customer customer){
         customer.greetCustomer();
+        int countPrice = calculateTotalPrice(customer.basket);
+        System.out.println(countPrice);
     }
 
     /**
      * Calculate the total price.
+     * Voor een tellertje tot size van je lijst tel alle prijzen bij elkaar op
      */
     public int calculateTotalPrice(ArrayList<Product> basket) {
-        return 0;
+        int countPrice = 0;
+        for (int i = 0; i < basket.size(); i++){
+            countPrice += basket.get(i).getPriceProduct();
+        } return countPrice;
     }
 
 
